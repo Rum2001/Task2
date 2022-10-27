37c7977a3a1e15e61edd5ff3dc44c135e7782a46
Rum2001 <61676156+Rum2001@users.noreply.github.com>
2022-10-27 16:36:11 +0700

    commit main

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
-// var slideIndex = 0;
-// carousel();
+const slideItems = [{
+  bgColor: "var(--color-bg1)"
+},
+{
+  bgColor: "var(--color-bg2)"
+},
+{
+  bgColor: "var(--color-bg3)"
+},
+{
+  bgColor: "var(--color-bg4)"
+},
+{
+  bgColor: "var(--color-bg5)"
+}, {
+  bgColor: "var(--color-bg6)"
+}, {
+  bgColor: "var(--color-bg7)"
+}, {
+  bgColor: "var(--color-bg8)"
+}
+]
+var slideIndex = 0;
+var x = document.getElementsByClassName("banner-slide");
+var y = document.getElementById("bg-color");
+carousel();
+function carousel() {
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
 
-// function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("banner-slide");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none"; 
-//   }
-//   slideIndex++;
-//   if (slideIndex > x.length) {slideIndex = 1} 
-//   x[slideIndex-1].style.display = "block"; 
-//   setTimeout(carousel, 2000); 
-// }
+  }
+  slideIndex++;
+  if (slideIndex > x.length) { slideIndex = 1 }
+  y.style.backgroundColor = slideItems[slideIndex - 1].bgColor;
+  x[slideIndex - 1].style.display = "block";
+
+  setTimeout(carousel, 5000);
+}
+
+
